aae1389c6c7a894542387b9aff1a8e7d7f17f42f
om1907 <98704784+om1907@users.noreply.github.com>
2024-02-22 16:39:49 +0530

    Update server.js

--- server.js
+++ server.js
@@ -4,7 +4,9 @@ const mongoose=require('mongoose');
 
 const PORT=process.env.PORT||3000;
 
-
+app.get('/',(req,res)=>{
+    res.send('main branch home route');
+})
 
 app.listen(PORT,()=>{
     console.log(`Server is running on http://localhost:${PORT}`)
