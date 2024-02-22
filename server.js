14da99e31baa2bdfe89aaba4fee9e61d61dc24bf
Pandey <opandey1@TAGICSUs-MacBook-Air-92.local>
2024-02-22 10:36:22 +0530

    new branch added

--- server.js
+++ server.js
@@ -1,8 +1,10 @@
-
 const express=require('express');
 const app=express();
+const mongoose=require('mongoose');
 
 const PORT=process.env.PORT||3000;
+
+
 
 app.listen(PORT,()=>{
     console.log(`Server is running on http://localhost:${PORT}`)
